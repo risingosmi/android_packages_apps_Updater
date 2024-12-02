abb3b441de0e11bd166f7b828f05e7ef8407e984
Alvin Francis <nivlafx@gmail.com>
2024-12-01 22:30:13 -0400

    Updater: Add floating buttons
    Signed-off-by: Alvin Francis <nivlafx@gmail.com>

--- app/src/main/java/com/rising/updater/UpdatesActivity.java
+++ app/src/main/java/com/rising/updater/UpdatesActivity.java
@@ -63,6 +63,7 @@ import androidx.recyclerview.widget.SimpleItemAnimator;
 
 import com.google.android.material.appbar.AppBarLayout;
 import com.google.android.material.appbar.CollapsingToolbarLayout;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
 import org.json.JSONException;
@@ -232,6 +233,11 @@ public class UpdatesActivity extends UpdatesListActivity implements UpdateImport
         }
 
         maybeShowWelcomeMessage();
+
+        FloatingActionButton fabRefresh = findViewById(R.id.fab_refresh);
+        fabRefresh.setOnClickListener(view -> downloadUpdatesList(true));
+        FloatingActionButton fabLocalUpdate = findViewById(R.id.fab_local_update);
+        fabLocalUpdate.setOnClickListener(view -> mUpdateImporter.openImportPicker());
     }
 
     @Override
@@ -550,7 +556,10 @@ public class UpdatesActivity extends UpdatesListActivity implements UpdateImport
 
     @Override
     public void showSnackbar(int stringId, int duration) {
-        Snackbar.make(findViewById(R.id.main_container), stringId, duration).show();
+        Snackbar snackbar = Snackbar.make(findViewById(R.id.main_container), stringId, duration);
+        snackbar.setAnchorView(R.id.fab_refresh);
+        snackbar.show();
+        
     }
 
     private void refreshAnimationStart() {
